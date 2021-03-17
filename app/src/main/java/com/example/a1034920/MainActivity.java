d7380b6c622837e3b44621d6c9fe21a9619df8a2
Dennis Mwangi <dennistrevor06@gmail.com>
2021-03-17 22:40:06 +0300

    initializing media player

--- app/src/main/java/com/example/a1034920/MainActivity.java
+++ app/src/main/java/com/example/a1034920/MainActivity.java
@@ -17,50 +17,70 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.sound_file_1);
-        mediaPlayer.start(); // no need to call prepare(); create() does that for you
-        Uri myUri = ....; // initialize Uri here
         private static final String VIDEO_SAMPLE = "tacoma_narrows";
         private VideoView mVideoView;
         private Uri getMedia(String mediaName) {
-        return Uri.parse("android.resource://" + getPackageName() + 
-        "/raw/" + mediaName);
-            private void initializePlayer() {
-                Uri videoUri = getMedia(VIDEO_SAMPLE);
-                mVideoView.setVideoURI(videoUri);
-                mVideoView.start();
-                private void releasePlayer() {
-                mVideoView.stopPlayback();
-}
-}
-}
-        @Override
-protected void onStart() {
-   super.onStart();
+            return Uri.parse("android.resource://" + getPackageName() +
+                    "/raw/" + mediaName);
+            private Uri getMedia(String mediaName) {
+                return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
+                mvideoView = findViewById(R.id.videoview);
+                private void initializePlayer() {
+                    Uri videoUri = getMedia(VIDEO_SAMPLE);
+                    mVideoView.setVideoURI(videoUri);
+                    mVideoView.start();
+                    private void releasePlayer() {
+                        mVideoView.stopPlayback();
+                        @Override
+                        protected  void onStop() {
+                            super.onStart();
+                            initializePlayer();
+                        }
+                        @Override
+                        protected void onStop() {
+                            super.onStop();
 
-   initializePlayer();
-}
-        @Override
-protected void onStop() {
-   super.onStop();
+                            releasePlayer();
+                        }
+                        @Override
+                        protected void onPause() {
+                            super.onPause();
 
-   releasePlayer();
-}
-        @Override
-protected void onPause() {
-   super.onPause();
+                            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                                mVideoView.pause();
+                            }
+                        }
+                        MediaController controller = new MediaController(this);
+                        controller.setMediaPlayer(mVideoView);
+                        mVideoView.setMediaController(controller);
+                        private int mCurrentPosition = 0;
+                        private static final String PLAYBACK_TIME = "play_time";
+                        if (mCurrentPosition > 0) {
+                            mVideoView.seekTo(mCurrentPosition);
+                        } else {
+                            // Skipping to 1 shows the first frame of the video.
+                            mVideoView.seekTo(1);
+                        }
+                        @Override
+                        protected void onSaveInstanceState(Bundle outState) {
+                            super.onSaveInstanceState(outState);
 
-   if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-       mVideoView.pause();
-   }
-}
-   MediaController controller = new MediaController(this);
-controller.setMediaPlayer(mVideoView);
-  mVideoView.setMediaController(controller);
-
-// ...prepare and start...
+                            outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+                        }
+                        if (savedInstanceState != null) {
+                            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+                        }
+                        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mediaPlayer) {
+                                // Implementation here.
+                            }
+                        });
+                    }
+                }
+            }
+        }
     }
+
 
 }
